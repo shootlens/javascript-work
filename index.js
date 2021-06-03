2b98bce634799683c3d0adb88d177ff0356c14e2
shootlens <raghavendraachari7676@gmail.com>
2021-06-03 14:29:25 +0530

    javascript work

---
+++ index.js
@@ -0,0 +1,4 @@
+alert("raghav");
+document.querySelector("h1").style= "color:green";
+document.getElementsByTagName("li")[2].style.color = "green";
+document.querySelector("li a").style.color = "pink";
